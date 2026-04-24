585eb576d42a496d8c52c2552cb3e0ec74d9c1fc
archuna11 <146524658+archuna11@users.noreply.github.com>
2026-04-24 18:16:52 +0530

    Removed unused scripts

--- migrate.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("instance/app.db")
-cursor = conn.cursor()
-cursor.execute("ALTER TABLE consultation ADD COLUMN doctor_id INTEGER;")
-conn.commit()
-conn.close()
-print("Column added successfully")
